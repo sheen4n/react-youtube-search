c048f3508174f0216428c3f2b3bc777784052ece
sheen4n <sheen.an.goh@gmail.com>
2020-03-05 14:37:43 +0800

    Updates

---
+++ precache-manifest.1cfead8fa7f701411d8f7a77d1c9f8d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4672eeceb4739a5a798145fa9b72c7",
+    "url": "/react-youtube-search/index.html"
+  },
+  {
+    "revision": "258b4c488adfad848e6d",
+    "url": "/react-youtube-search/static/css/main.24e5d9ee.chunk.css"
+  },
+  {
+    "revision": "8054f6fffd598433ecfc",
+    "url": "/react-youtube-search/static/js/2.a65151ab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-youtube-search/static/js/2.a65151ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258b4c488adfad848e6d",
+    "url": "/react-youtube-search/static/js/main.aea8e464.chunk.js"
+  },
+  {
+    "revision": "e69df95b24542bd86be2",
+    "url": "/react-youtube-search/static/js/runtime-main.f6273510.js"
+  }
+]);
